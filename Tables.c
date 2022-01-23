21687fa01db414e01324dbff9ac61c8ac68d6a3f
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-01-23 21:21:51 +0530

    #include<stdio.h> int main() { 	int N,R,i; 	scanf("%d%d",&N,&R); 	for(i=1;i<=R;i++) 	{ 		if(i%2==0) 		{ 		    continue; 		} 		printf("%d x %d = %d ",N,i,N*i); 	} 	return 0; }

---
+++ Tables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int N,R,i;
+	scanf("%d%d",&N,&R);
+	for(i=1;i<=R;i++)
+	{
+		if(i%2==0)
+		{
+		    continue;
+		}
+		printf("%d x %d = %d
+",N,i,N*i);
+	}
+	return 0;
+}
