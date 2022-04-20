32888101012a148752a9a9629f819d90017091f7
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-04-20 22:29:10 +0530

    #include<stdio.h> int main() {     int n,d,res=0;     scanf("%d",&n);     while(n)     {         d=n%10;         res=res*10 +d;         n=n/10;     }     printf("%d",res); }

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,s,d,res=0;
+    int n,d,res=0;
     scanf("%d",&n);
     while(n)
     {
         d=n%10;
-        res=res*10+d;
-      
+        res=res*10 +d;
         n=n/10;
     }
-      printf("%d",res);
-    return 0;
+    printf("%d",res);
 }
