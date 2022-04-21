46d043d21b218c44aa54fdda40dd98a33e595711
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-04-21 21:04:42 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,es=0,os=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             es+=arr[i];         }         else         {             os+=arr[i];         }     }     if(es>os)     {         printf("%d",es-os);     }     else     {         printf("%d",os-es);     } }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,es=0,os=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            es+=arr[i];
+        }
+        else
+        {
+            os+=arr[i];
+        }
+    }
+    if(es>os)
+    {
+        printf("%d",es-os);
+    }
+    else
+    {
+        printf("%d",os-es);
+    }
+}
