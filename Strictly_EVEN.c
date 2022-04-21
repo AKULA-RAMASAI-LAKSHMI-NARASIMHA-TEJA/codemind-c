8efd58de6a148c48cdb8a60c4e8688b609f4970e
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-04-21 22:46:26 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,e=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             e++;         }         if(i%2==0 && arr[i]%2==0)         {             c++;         }     }     if(e==c)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -1,44 +1,28 @@
 #include<stdio.h>
-int strictly_even(int *a,int n)
-{
-    int i,e=0,ei=0;
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2==0)
-        {
-            e++;
-        }
-    }
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2==0 && i%2==0)
-        {
-            ei++;
-        }
-    }
-    if(e==ei)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
 int main()
 {
-    int a[100],x,n,i;
+    int n,arr[100],i,e=0,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
-    x=strictly_even(a,n);
-    if(x==1)
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            e++;
+        }
+        if(i%2==0 && arr[i]%2==0)
+        {
+            c++;
+        }
+    }
+    if(e==c)
     {
         printf("True");
     }
-    if(x==0)
+    else
     {
         printf("False");
     }
