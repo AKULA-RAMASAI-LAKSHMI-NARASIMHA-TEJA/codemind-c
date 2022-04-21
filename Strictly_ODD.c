db8fe4d4443ec72e8ee218164c806561d43191dd
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-04-21 22:47:47 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,o=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             o++;         }         if(i%2!=0 && arr[i]%2!=0)         {             c++;         }     }     if(o==c)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,44 +1,28 @@
 #include<stdio.h>
-int strictly_odd(int *a,int n)
-{
-    int i,o=0,oi=0;
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2!=0)
-        {
-            o++;
-        }
-    }
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2!=0 && i%2!=0)
-        {
-            oi++;
-        }
-    }
-    if(o==oi)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
 int main()
 {
-    int a[100],x,n,i;
+    int n,arr[100],i,o=0,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
-    x=strictly_odd(a,n);
-    if(x==1)
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            o++;
+        }
+        if(i%2!=0 && arr[i]%2!=0)
+        {
+            c++;
+        }
+    }
+    if(o==c)
     {
         printf("True");
     }
-    if(x==0)
+    else
     {
         printf("False");
     }
