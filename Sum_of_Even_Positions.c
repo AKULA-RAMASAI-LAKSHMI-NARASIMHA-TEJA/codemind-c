02f1f12eb887fdf1acf7a0aaf47cd37f6378c85e
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-04-21 21:13:49 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,es=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             es+=arr[i];         }     }     printf("%d",es);
    }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,es=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            es+=arr[i];
+        }
+    }
+    printf("%d",es);
+    
+}
