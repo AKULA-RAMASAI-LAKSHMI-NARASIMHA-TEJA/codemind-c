bfb4f7fdb07c3ad97d3135c0966bcec5c376e38e
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-04-21 22:20:06 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,num;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             num=arr[i];         }     }     printf("%d",num); }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,num;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            num=arr[i];
+        }
+    }
+    printf("%d",num);
+}
