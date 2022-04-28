7e092e6af5f2b45b47b78f1fae25d7f66e93b879
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-04-28 19:03:57 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n-1;i++)     {         if(arr[i]>arr[i+1])         {             c++;         }     }     if(c==n-1)     {         printf("yes");     }     else     {         printf("no");     } }

--- Descending_array.c
+++ Descending_array.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],c=0;
+    int n,arr[100],i,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<n-1;i++)
     {
         if(arr[i]>arr[i+1])
         {
@@ -16,10 +16,10 @@ int main()
     }
     if(c==n-1)
     {
-        printf("no");
+        printf("yes");
     }
     else
     {
-        printf("yes");
+        printf("no");
     }
 }
